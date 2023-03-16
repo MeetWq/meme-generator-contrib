65f4e6f5c377bba80e1ffeeb7f3d212a31a8ed9b
meetwq <meetwq@gmail.com>
2023-03-16 15:48:29 +0800

    看看你的 调整顺序

--- memes/can_can_need/__init__.py
+++ memes/can_can_need/__init__.py
@@ -10,9 +10,9 @@ img_dir = Path(__file__).parent / "images"
 def can_can_need(images: List[BuildImage], texts, args):
     frame = BuildImage.open(img_dir / "0.jpg")
     frame.paste(
-        images[0].convert("RGBA").circle().resize((340, 340)), (120, 21), alpha=True
+        images[1].convert("RGBA").circle().resize((340, 340)), (120, 21), alpha=True
     ).paste(
-        images[1].convert("RGBA").circle().resize((300, 300)), (611, 718), alpha=True
+        images[0].convert("RGBA").circle().resize((300, 300)), (611, 718), alpha=True
     )
     return frame.save_jpg()
 
